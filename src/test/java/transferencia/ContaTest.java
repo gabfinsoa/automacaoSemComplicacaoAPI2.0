cb4d0d9ce8affdb328866a1679346edbaa96b5c8
Gabriel Soares <gabfinsoa@gmail.com>
2021-12-13 20:27:52 +0000

    First_Test_BeforeEach_SetUp

--- src/test/java/transferencia/ContaTest.java
+++ src/test/java/transferencia/ContaTest.java
@@ -1,7 +1,47 @@
 package transferencia;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ContaTest {
+
+    Cliente xuxa;
+    Cliente silvioSantos;
+    Conta contaXuxa;
+    Conta contaSilvio;
+
+
+    //Before Each Test   Use: ALT + INSERT
+    @BeforeEach
+    void setUp() {
+        xuxa = new Cliente("Xuxa", "12345678900", "11111");
+        silvioSantos = new Cliente("Silvio Santos", "00987654321", "22222");
+
+        contaXuxa = new Conta("0025", "2254", 2500, xuxa);
+        contaSilvio = new Conta("0025", "33208", 3500, silvioSantos);
+    }
+
+    @Test
+    public void realizarTransacao() {
+        contaXuxa.realizarTransferencia(1000, contaSilvio);
+
+        assertEquals(1500, contaXuxa.getSaldo());
+        assertEquals(4500, contaSilvio.getSaldo());
+    }
+
+    @Test
+    public void validarTransferenciaInvalida() {
+        boolean resultado = contaXuxa.realizarTransferencia(3500, contaSilvio);
+
+        assertFalse(resultado);
+    }
+
+    @Test
+    public void validarProprietario() {
+        assertEquals(xuxa, contaXuxa.getProprietario());
+
+    }
 
 }

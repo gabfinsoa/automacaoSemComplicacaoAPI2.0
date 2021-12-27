231330a8489abe39916c031e73ce509da6a1efa4
Gabriel Soares <gabfinsoa@gmail.com>
2021-12-27 22:58:26 +0000

    aula09_map_get beforeAll

--- src/test/java/plataformaFilmes/PlataformaFilmesTest.java
+++ src/test/java/plataformaFilmes/PlataformaFilmesTest.java
@@ -3,11 +3,17 @@ package plataformaFilmes;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlataformaFilmesTest {
+
+    static String token;
 
     @Test
     public void validarLogin() {
@@ -27,7 +33,42 @@ public class PlataformaFilmesTest {
         System.out.println("TOKEN: " + pegaToken);
     }
 
-    public Response post(Object jsonBody, ContentType contentType, String endpoint) {
+    @BeforeAll
+    public static void validarLoginMap() {
+        RestAssured.baseURI = "http://localhost:8080/";
+
+        Map<String, String> map = new HashMap<>(); //Tipo da CHAVE e o tipo do VALOR. Sugest.: VALOR pode ser do tipo OBJETO
+        map.put("email", "aluno@email.com"); // Add novo CHAVE e VALOR
+        map.put("senha", "123456");
+
+        Response response = post(map, ContentType.JSON, "auth");
+
+        assertEquals(200, response.statusCode());
+        token = response.jsonPath().get("token");
+        System.out.println(token);
+    }
+
+    @Test
+    public void validarConsultaCategorias() {
+        Map<String, String> header = new HashMap<>();
+        header.put("Authorization", "Bearer " + token);
+
+        Response response = get(header, "categorias"); //CTRL+ALT+M -> Cria o método. (neste caso o "private get" abaixo)
+
+        assertEquals(200, response.statusCode());
+        System.out.println(response.jsonPath().get().toString()); //Printa o JSON BODY
+    }
+
+    private Response get(Map<String, String> header, String endpoint) {
+        return RestAssured.given()
+                .relaxedHTTPSValidation()
+                .headers(header)  //HEADERS com "s" para poder enviar o MAP (chave, valor)
+                .when()
+                .get(endpoint)
+                .thenReturn();
+    }
+
+    public static Response post(Object jsonBody, ContentType contentType, String endpoint) {
         return RestAssured.given()  //Retorna o RESPONSE
                 .relaxedHTTPSValidation() //Passa por HTTPS e Certificados
                 .contentType(contentType)
